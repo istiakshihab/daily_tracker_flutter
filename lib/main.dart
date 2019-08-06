1058c6035299cef36d7f030b15af932f64e61678
istiakshihab <shihabistiak@gmail.com>
2019-08-06 15:41:03 +0600

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main(){
-
-}
+void main()=> runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text("Flutter Testing"),),
+      body: ,
+    ),);
   }
 }
