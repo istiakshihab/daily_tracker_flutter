824be9cd8d5c6c61e8143f4b7039360441af551c
istiakshihab <shihabistiak@gmail.com>
2019-08-07 14:56:16 +0600

    Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIdx = 0;
+  var questions = [
+    "What's your favorite color?",
+    "What's your favorite animal?",
+  ];
+
+  void answerQuestions() {
+    questionIdx++;
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text("Flutter Testing"),),
-      body: ,
-    ),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Testing"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIdx]),
+            RaisedButton(
+              child: Text('Fuckface 1'),
+              onPressed: answerQuestions,
+            ),
+            RaisedButton(
+              child: Text('Fuckface 2'),
+              onPressed: answerQuestions,
+            ),
+            RaisedButton(
+              child: Text('Fuckface 3'),
+              onPressed: answerQuestions,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
